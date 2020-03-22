ac4337cb309b5ecb97e9cc8a25c82287af84b040
vall4251n <58278138+nvalline@users.noreply.github.com>
2020-03-22 11:24:57 -0600

    added psuedo code to js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,6 +4,7 @@ const time = document.getElementById("time");
 const timeDisplay = document.getElementById("time-display");
 const startBtn = document.getElementById("start-btn");
 
+// Timer Function
 function startTimer(event) {
     let timeLeft = 11;
     let quizTimer = setInterval(function () {
@@ -15,5 +16,23 @@ function startTimer(event) {
     }, 1000);
 }
 
+// start clicked => display Q1, start timer
+
+// if correct clicked => display Correct
+// if correct add to score
+// if wrong clicked => display Wrong
+// if wrong subtract time
+// hide Q1 => display Q2
+// repeat
+
+// leaderboard hides divider & answer block
+// after Q5 display leaderboard with input
+// after input submitted hide input
+
+// if time = 0, game ends and displays leaderboard
+
+// if View Highscores clicked display leaderboard w/o input => Play Again button displays Take Quiz
+
+// if Play Again clicked return to Start Quiz
 
 startBtn.addEventListener("click", startTimer);
