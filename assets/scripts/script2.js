b163d966cf378d8e6d0df986cc8475e63d4aafe9
vall4251n <58278138+nvalline@users.noreply.github.com>
2020-03-23 08:51:35 -0600

    added questions & answers to js

--- assets/scripts/script2.js
+++ assets/scripts/script2.js
@@ -21,12 +21,46 @@ function startQuiz() {
 
 const questions = [
     {
-        question: "",
+        question: "An ID that has been created in your CSS can be used __________ time(s) within you HTML document?",
         answers: [
-            { text: "", correct: true },
-            { text: "", correct: false },
-            { text: "", correct: false },
-            { text: "", correct: false },
+            { text: "Four", correct: false },
+            { text: "Three", correct: false },
+            { text: "Two", correct: false },
+            { text: "One", correct: true }
+        ]
+    },
+    {
+        question: "True or false? You should avoid coding inline styles within an HTML document whenever possible?",
+        answers: [
+            { text: "True", correct: true },
+            { text: "False", correct: false }
+        ]
+    },
+    {
+        question: "Which of the following tags would allow you to input a line break in your HTML document correctly?",
+        answers: [
+            { text: "Br", correct: true },
+            { text: "B", correct: false },
+            { text: "Hr", correct: false },
+            { text: "P", correct: false }
+        ]
+    },
+    {
+        question: "This section code holds the most of the page's information or content?",
+        answers: [
+            { text: "HTML", correct: false },
+            { text: "BODY", correct: true },
+            { text: "TITLE", correct: false },
+            { text: "HEAD", correct: false }
+        ]
+    },
+    {
+        question: "A ______________ is a set of text or button hyperlinks that can be used to access pages in your web site?",
+        answers: [
+            { text: "Route Bar", correct: false },
+            { text: "Map Bar", correct: false },
+            { text: "Direction Bar", correct: false },
+            { text: "Navigation Bar", correct: true }
         ]
     }
 ]
