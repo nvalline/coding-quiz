e2f7643f5aa720e1e1a3c51b4b26369058922398
vall4251n <58278138+nvalline@users.noreply.github.com>
2020-03-23 18:05:59 -0600

    deleted old js file

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,141 +1,237 @@
-const quizBlock = document.getElementById("quiz-block");
-const yourScore = document.getElementById("your-score");
-const leaderBlock = document.getElementById("leader-block");
-const leaderboard = document.getElementById("leaderboard");
-const leaderForm = document.getElementById("leader-form");
-const submitBtn = document.getElementById("submit-btn");
-const time = document.getElementById("time");
-const timeDisplay = document.getElementById("time-display");
-const startBtn = document.getElementById("start-btn");
-const answerTimeBlock = document.getElementById("answer-time-block");
-const questionOne = document.getElementById("question-one");
-const questionBlock = document.querySelectorAll(".question-block");
-const answerBtns = document.querySelectorAll(".answer-btn");
-const answerStatus = document.getElementById("answer-status");
-const viewHighScores = document.getElementById("view-highscores");
-const nameInput = document.getElementById("name-input");
-const playAgainBtn = document.getElementById("play-again");
+const startBtn = document.getElementById('start-btn')
+const startContainer = document.getElementById('start-container')
+const questionContainer = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+const answerTimeContainer = document.getElementById('answer-time-container')
+const endScreenContainer = document.getElementById('end-screen-container')
+const scoreElement = document.getElementById('your-score')
+const answerStatusElement = document.getElementById('answer-status')
+const timeDisplayElement = document.getElementById('time-display')
+const submitBtn = document.getElementById('submit-btn')
+const leaderboardContainer = document.getElementById('leaderboard-container')
+const leaderName = document.getElementById('leader-input')
+const playAgainBtn = document.getElementById('play-again')
+const leaderboardList = document.getElementById('leaderboard-list')
+const viewHighscoresElement = document.getElementById('view-highscores')
 
-const leaders = {};
-const initialTime = 60;
-let timeLeft = initialTime;
-let score = 0;
+const questions = [
+    {
+        question: "An ID that has been created in your CSS can be used __________ time(s) within you HTML document?",
+        answers: [
+            { text: "Four", correct: false },
+            { text: "Three", correct: false },
+            { text: "Two", correct: false },
+            { text: "One", correct: true }
+        ]
+    },
+    {
+        question: "True or false? You should avoid coding inline styles within an HTML document whenever possible?",
+        answers: [
+            { text: "True", correct: true },
+            { text: "False", correct: false }
+        ]
+    },
+    {
+        question: "Which of the following tags would allow you to input a line break in your HTML document correctly?",
+        answers: [
+            { text: "Br", correct: true },
+            { text: "B", correct: false },
+            { text: "Hr", correct: false },
+            { text: "P", correct: false }
+        ]
+    },
+    {
+        question: "This section code holds the most of the page's information or content?",
+        answers: [
+            { text: "HTML", correct: false },
+            { text: "BODY", correct: true },
+            { text: "TITLE", correct: false },
+            { text: "HEAD", correct: false }
+        ]
+    },
+    {
+        question: "A ______________ is a set of text or button hyperlinks that can be used to access pages in your web site?",
+        answers: [
+            { text: "Route Bar", correct: false },
+            { text: "Map Bar", correct: false },
+            { text: "Direction Bar", correct: false },
+            { text: "Navigation Bar", correct: true }
+        ]
+    }
+]
 
-let currentQuestion = 0;
+const initialTime = 30
 
-// Display Questions
-function displayQuestions(event) {
-    // start timer
-    startTimer();
-    // hide start quiz
-    quizBlock.classList.add("hide");
-    // display answer block
-    answerTimeBlock.classList.remove("hide");
-    // hide highscore link
-    viewHighScores.classList.add("hide")
-    // display question block
-    questionOne.classList.remove("hide")
+let score = 0
 
+let currentQuestionIndex
+let nextQuestion
+let time = initialTime
+let quizTimer
+let finalScore
+
+let leaders = []
+
+startBtn.addEventListener('click', startQuiz)
+viewHighscoresElement.addEventListener('click', leaderboardShortcut)
+
+// start Quiz
+function startQuiz() {
+    startTimer()
+    startContainer.classList.add('hide')
+    questionContainer.classList.remove('hide')
+    currentQuestionIndex = questions[0]
+    answerTimeContainer.classList.remove('hide')
+    showNextQuestion()
 }
 
-for (var j = 0; j < questionBlock.length; j++) {
-    currentQuestion = questionBlock[j];
+function nextQuestionIndex() {
+    nextQuestion = questions.indexOf(currentQuestionIndex)
+    nextQuestion++
+    currentQuestionIndex = questions[nextQuestion]
 }
 
-
-// Display Highscores & Return to Start
-function displayHighScores(event) {
-    // hide start quiz
-    quizBlock.classList.add("hide");
-    // display highscores
-    leaderBlock.classList.remove("hide");
-    playAgainBtn.innerText = "Take Quiz";
-}
-
-// Return to start
-function returnToStart(event) {
-    // display start quiz
-    quizBlock.classList.remove("hide");
-    // hide highscores
-    leaderBlock.classList.add("hide");
-}
-
-// Show leaderboard with input & score
-function nameSubmit() {
-    questionOne.classList.add("hide");
-    answerTimeBlock.classList.add("hide");
-    nameInput.classList.remove("hide");
-    yourScore.childNodes[1].innerText = score;
-    timeLeft = initialTime;
-    answerStatus.innerText = "";
-}
-
-// Submit name to leaderboard
-function submitScore(event) {
-    submitBtn.preventDefault();
-    let name = document.getElementById("leader-input").value;
-    leaders.name = name;
-    leaders.score = score;
-    let newLeaderNode = document.createElement("li");
-    newLeaderNode.innerText = leaders.name + " - " + leaders.score;
-    leaderboard.appendChild(newLeaderNode);
-}
-
-// Determine answer and adjust score & time
-function displayAnswer(event) {
-    let correctAnswer = event.target.getAttribute("data-answer");
-
-    if (correctAnswer === "correct") {
-        answerStatus.textContent = "Correct!";
-        score = score + 10;
+function showNextQuestion() {
+    resetAnswerState()
+    answerStatusElement.innerText = ""
+    if (!currentQuestionIndex || time <= 1) {
+        clearInterval(quizTimer)
+        showEndScreen()
     } else {
-        answerStatus.textContent = "Wrong...";
-        if (timeLeft > 10) {
-            timeLeft = timeLeft - 10;
-        } else {
-            timeLeft = 1;
-        }
+        showQuestion(currentQuestionIndex)
+    }
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(showAnswerButtons)
+}
+
+function showAnswerButtons(answer) {
+    let button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+        button.dataset.correct = answer.correct
+    }
+
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+}
+
+function selectAnswer(event) {
+    let selectedButton = event.target
+    if (selectedButton.dataset.correct) {
+        answerStatusElement.textContent = "Correct!"
+        score = score + 10
+    } else {
+        // subtract time
+        answerStatusElement.textContent = "Wrong..."
+        time = time - 10
+    }
+
+    setTimeout(function () {
+        nextQuestionIndex()
+        showNextQuestion()
+    }, 1000)
+}
+
+function resetAnswerState() {
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
 }
 
 // Timer Function
 function startTimer(event) {
-    let quizTimer = setInterval(function () {
-        if (timeLeft <= 1) {
+    quizTimer = setInterval(function () {
+        if (time <= 1) {
             clearInterval(quizTimer);
-            // Trigger show leaderboard
-            nameSubmit();
+            // Trigger show end screen
+            showEndScreen();
         }
-        timeLeft -= 1;
-        timeDisplay.innerText = timeLeft;
+        time -= 1;
+        timeDisplayElement.innerText = time;
     }, 1000);
 }
 
-// save to local storage
-localStorage.setItem("leaders", JSON.stringify(leaders));
+function showEndScreen() {
+    questionContainer.classList.add('hide')
+    answerTimeContainer.classList.add('hide')
+    endScreenContainer.classList.remove('hide')
 
-// Event listener for Answer Buttons
-for (var i = 0; i < answerBtns.length; i++) {
-    answerBtns[i].addEventListener("click", displayAnswer, false);
+    if (time > 0) {
+        finalScore = score + "." + time
+    } else {
+        finalScore = score
+    }
+
+    scoreElement.innerText = finalScore
+    submitBtn.addEventListener('click', submitLeaderInfo)
 }
 
-// Global event listeners
-startBtn.addEventListener("click", displayQuestions);
-playAgainBtn.addEventListener("click", returnToStart);
-viewHighScores.addEventListener("click", displayHighScores);
-submitBtn.addEventListener("click", submitScore);
+function submitLeaderInfo(event) {
+    event.preventDefault()
+    let leaderText = leaderName.value.trim()
+    if (leaderText === "") {
+        return
+    }
+    leaderText = leaderText + " - " + finalScore
+    leaders.push(leaderText)
+    leaderName.value = ""
+    showLeaderboard()
+}
 
-// start clicked => display Q1, start timer
+function showLeaderboard(event) {
+    resetLeaderState()
+    endScreenContainer.classList.add('hide')
+    leaderboardContainer.classList.remove('hide')
+    timeDisplayElement.innerText = ""
+    leaders.forEach(showLeaders)
 
-// if correct clicked => display Correct
-// if correct add to score
-// if wrong clicked => display Wrong
-// if wrong subtract time
-// hide Q1 => display Q2
-// repeat
+    // restart game
+    playAgainBtn.addEventListener('click', resetQuiz)
+}
 
-// leaderboard hides divider & answer block
-// after Q5 display leaderboard with input
-// after input submitted hide input
+function resetQuiz() {
+    leaderboardContainer.classList.add('hide')
+    startContainer.classList.remove('hide')
+    currentQuestionIndex = questions[0]
+    nextQuestion = questions.indexOf(currentQuestionIndex)
+    score = 0
+    finalScore = 0
+    time = initialTime
+}
 
-// if time = 0, game ends and displays leaderboard
+function showLeaders(leader) {
+    let liElement = document.createElement('li')
+    liElement.innerText = leader
+    leaderboardList.appendChild(liElement)
+}
+
+function resetLeaderState() {
+    while (leaderboardList.firstChild) {
+        leaderboardList.removeChild(leaderboardList.firstChild)
+    }
+}
+
+function leaderboardShortcut() {
+    startContainer.classList.add('hide')
+    leaderboardContainer.classList.remove('hide')
+
+    playAgainBtn.addEventListener('click', resetQuiz)
+}
+// display first question & answers
+
+// determine if correct
+
+// advance to next question
+
+// log score
+
+// set timer
+
+// after last question advance to input
+
+// after input advance to leaderboard
